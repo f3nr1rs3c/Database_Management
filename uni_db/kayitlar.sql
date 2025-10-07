2b81387442b76a10ebbf06e4b032a5a02318321b
Doğukan ISPIRLI <51451598+f3nr1rs3c@users.noreply.github.com>
2025-10-07 18:23:42 +0300

    Create kayitlar.sql

---
+++ uni_db/kayitlar.sql
@@ -0,0 +1,9 @@
+CREATE TABLE kayitlar (
+    kayit_id INT AUTO_INCREMENT PRIMARY KEY,
+    ogrenci_id INT NOT NULL,
+    ders_kodu VARCHAR(10) NOT NULL,
+    notu DECIMAL(4,2),
+    FOREIGN KEY (ogrenci_id) REFERENCES ogrenciler(ogr_no),
+    FOREIGN KEY (ders_kodu) REFERENCES dersler(ders_kodu)
+)
+ENGINE = InnoDB;
